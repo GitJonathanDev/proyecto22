8bd7ac7f569969e6e9f277d4d0cc4bbd9db2f2d4
GitJonathanDev <144186353+GitJonathanDev@users.noreply.github.com>
2024-11-28 16:29:48 -0400

    jijoffssccccdfdddcfccfdffgcddfvf

--- app/Models/DetalleCompra.php
+++ app/Models/DetalleCompra.php
@@ -9,19 +9,18 @@ class DetalleCompra extends Model
 {
     use HasFactory;
 
-    // Nombre de la tabla
+
     protected $table = 'DetalleCompra';
 
-    // Desactiva las claves primarias por defecto
-    protected $primaryKey = null;
+  
+    protected $primaryKey = ['codCompra', 'codProducto'];
 
-    // Clave no incremental
+
     public $incrementing = false;
 
-    // Desactiva las marcas de tiempo
+
     public $timestamps = false;
 
-    // Atributos que se pueden asignar en masa
     protected $fillable = [
         'precioC',
         'cantidad',
@@ -29,37 +28,23 @@ class DetalleCompra extends Model
         'codProducto',
     ];
 
-    // Conversión de atributos
+
     protected $casts = [
-        'codProducto' => 'string', // Garantiza que siempre se trate como string
+        'codProducto'=>'string',
         'precioC' => 'float',
         'cantidad' => 'integer',
         'codCompra' => 'integer',
     ];
 
-    // Relación con Compra
+
     public function compra()
     {
         return $this->belongsTo(Compra::class, 'codCompra', 'codCompra');
     }
 
-    // Relación con Producto
+
     public function producto()
     {
         return $this->belongsTo(Producto::class, 'codProducto', 'codProducto');
-    }
-
-    // Sobreescribe el método `getKey` para manejar la clave primaria compuesta
-    public function getKey()
-    {
-        return [$this->codCompra, $this->codProducto];
-    }
-
-    // Define un método para encontrar registros usando claves compuestas
-    public static function findComposite($codCompra, $codProducto)
-    {
-        return self::where('codCompra', $codCompra)
-            ->where('codProducto', $codProducto)
-            ->first();
     }
 }
